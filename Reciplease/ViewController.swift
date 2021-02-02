41cdcdf2a59752b81f0f1b0158df68b92a2080c7
Sergio Canto <sergio@bellcantolai.com>
2021-02-02 19:45:47 +0100

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Sergio Canto  on 02/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
